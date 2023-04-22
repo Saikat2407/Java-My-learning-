4bedec2bf4345757203b826dd201bb67c6821507
Saikat <s.nayek2407@gmail.com>
2023-04-22 19:06:16 +0530

    leetcode question on linked-list

--- src/saikat/SL/LL.java
+++ src/saikat/SL/LL.java
@@ -125,6 +125,54 @@ public class LL {
         return prevVal;
     }
 
+    //LEETCODE QUESTION
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-list
+
+    public void duplicate(){
+        Node node = head;
+
+        while (node.next != null){
+            if(node.value == node.next.value){
+                node.next = node.next.next;
+                size--;
+            }else {
+                node = node.next;
+            }
+        }
+        tail = node;
+        tail.next = null;
+    }
+
+    // https://leetcode.com/problems/merge-two-sorted-lists/submissions/
+
+    public static LL merge(LL first, LL second){
+        Node f = first.head;
+        Node s = second.head;
+
+        LL ans = new LL();
+
+        while (f != null && s != null){
+            if(f.value < s.value){
+                ans.insertLast(f.value);
+                f = f.next;
+            } else {
+                ans.insertLast(s.value);
+                s = s.next;
+            }
+        }
+        while (f != null) {
+            ans.insertLast(f.value);
+            f = f.next;
+        }
+
+        while (s != null) {
+            ans.insertLast(s.value);
+            s = s.next;
+        }
+
+        return ans;
+    }
+
     public Node returntheNode(int value){
         Node node = head;
         while (node != null){
